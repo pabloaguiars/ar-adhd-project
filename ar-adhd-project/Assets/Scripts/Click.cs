746ae5572d933f432de0ba88e9e2c3ca82bab925
Raul Godoy <raulgodoy97@hotmail.com>
2020-10-21 20:12:37 -0700

    feat:agregar proyecto minijuego1

---
+++ ar-adhd-project/Assets/Scripts/Click.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Click : MonoBehaviour
+{
+    public GameObject TextBox,message;
+    string nombre;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nombre = gameObject.name;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       
+    }
+
+    public void OnMouseDown()
+    {
+        if (nombre == TextBox.GetComponent<Text>().text)
+        {
+            message.GetComponent<Text>().text = "Bien hecho";
+            Destroy(gameObject);
+        }
+        else
+        {
+            message.GetComponent<Text>().text = "Uff cerca!";
+        }
+    }
+}
---
+++ ar-adhd-project/Assets/Scripts/SeleccionaObjeto.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SeleccionaObjeto : MonoBehaviour
+{
+    public GameObject TextBox;
+    public int numero;
+    string[] figuras = new string[6];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        figuras[0] = "Cubo";
+        figuras[1] = "Esfera";
+        figuras[2] = "Cilindro";
+        numero = Random.Range(0, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextBox.GetComponent<Text>().text = figuras[numero];
+    }
+}
