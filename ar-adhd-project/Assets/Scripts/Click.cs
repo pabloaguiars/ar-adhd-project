b54b65836f2772530fb05104ae2b14f4bd233906
Raul Godoy <raulgodoy97@hotmail.com>
2020-10-29 12:45:58 -0700

    feat: Posicionamiento de objetos aleatorios y vision 360

--- ar-adhd-project/Assets/Scripts/Click.cs
+++ ar-adhd-project/Assets/Scripts/Click.cs
@@ -8,16 +8,29 @@ public class Click : MonoBehaviour
 {
     public GameObject TextBox,message;
     string nombre;
+    private float posicionX, posicionY;
+    private float[] posicionZ = new float[6];
+
     // Start is called before the first frame update
     void Start()
     {
+        posicionX = Random.Range(3f, 6f);
+        posicionY = Random.Range(0f, 2f);
+        posicionZ[0] = 5;
+        posicionZ[1] = 6;
+        posicionZ[2] = 7;
+        posicionZ[3] = -5;
+        posicionZ[4] = -6;
+        posicionZ[5] = -7;
+
         nombre = gameObject.name;
+        transform.position = new Vector3(Random.Range(-posicionX,posicionX), Random.Range(-posicionY,posicionY), posicionZ[Random.Range(0,5)]);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-       
+
     }
 
     public void OnMouseDown()
