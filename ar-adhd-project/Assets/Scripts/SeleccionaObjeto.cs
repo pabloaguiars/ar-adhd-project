876e75af06201c574e13b4be2edbb233b0b1a26e
Raul Godoy <raulgodoy97@hotmail.com>
2020-11-08 16:48:40 -0800

    feat: Agregado de modelos

--- ar-adhd-project/Assets/Scripts/SeleccionaObjeto.cs
+++ ar-adhd-project/Assets/Scripts/SeleccionaObjeto.cs
@@ -5,18 +5,18 @@ using UnityEngine.UI;
 
 public class SeleccionaObjeto : MonoBehaviour
 {
-    public GameObject TextBox;
+    public GameObject Objetivo;
     public int numero;
     string[] figuras = new string[4];
 
     // Start is called before the first frame update
     void Start()
     {
-        figuras[0] = "Cubo";
-        figuras[1] = "Esfera";
+        figuras[0] = "Candy";
+        figuras[1] = "Chocolate";
         figuras[2] = "Paleta";
         numero = Random.Range(0, 3);
-        TextBox.GetComponent<Text>().text = figuras[numero];
+        Objetivo.GetComponent<Text>().text = figuras[numero];
     }
 
     // Update is called once per frame
--- ar-adhd-project/Assets/Scripts/cantidadObjetos.cs
+++ ar-adhd-project/Assets/Scripts/cantidadObjetos.cs
@@ -5,48 +5,50 @@ using UnityEngine.UI;
 
 public class cantidadObjetos : MonoBehaviour
 {
-    public GameObject esferas, paleta, cubo;
+    public GameObject chocolate, paleta, candy;
     private Vector3 posicion;
     private float posicionX, posicionY;
     private float[] posicionZ = new float[6];
-    private int cantidadEsferas, cantidadPaletas, cantidadCubos, MaxObjetos;
+    private int cantidadChocolates, cantidadPaletas, cantidadCandys, MaxObjetos;
     private string nombre;
-
+  
     // Start is called before the first frame update
     void Start()
     {
-        posicionX = Random.Range(3f, 6f);
-        posicionY = Random.Range(0f, 3f);
-        posicion = new Vector3(Random.Range(-posicionX, posicionX), Random.Range(-posicionY, posicionY), posicionZ[Random.Range(0, 5)]);
-        posicionZ[0] = 5;
-        posicionZ[1] = 6;
-        posicionZ[2] = 7;
-        posicionZ[3] = -5;
-        posicionZ[4] = -6;
-        posicionZ[5] = -7;
+
+        //posicionX = Random.Range(0f, 6f);
+        //posicionY = Random.Range(0f, 3f);
+        //posicion = new Vector3(Random.Range(-posicionX, posicionX), Random.Range(-posicionY, posicionY), posicionZ[Random.Range(0, 5)]);
+        //posicionZ[0] = 5;
+        //posicionZ[1] = 6;
+        //posicionZ[2] = 7;
+        //posicionZ[3] = -5;
+        //posicionZ[4] = -6;
+        //posicionZ[5] = -7;
 
         MaxObjetos = 20;
-        cantidadEsferas = Random.Range(3, MaxObjetos);
+        cantidadChocolates = Random.Range(3, MaxObjetos);
         cantidadPaletas = Random.Range(3, MaxObjetos);
-        cantidadCubos = Random.Range(3, MaxObjetos);
+        cantidadCandys = Random.Range(3, MaxObjetos);
 
+        posicion = Random.insideUnitSphere;
         nombre = gameObject.name;
 
 
-        for (int conta = 1; conta < cantidadEsferas; conta++)
+        for (int conta = 1; conta < cantidadChocolates; conta++)
         {
-            GameObject Esferas = Instantiate(esferas, posicion, Quaternion.identity);
-            Esferas.name = "Esfera";
+            GameObject Chocolate = Instantiate(chocolate, posicion, Quaternion.identity);
+            Chocolate.name = "Chocolate";
         }
         for (int conta2 = 1; conta2 < cantidadPaletas; conta2++)
         {
             GameObject Paleta = Instantiate(paleta, posicion, Quaternion.Euler(-89,-43,43));
             Paleta.name = "Paleta";
         }
-        for (int conta3 = 1; conta3 < cantidadCubos; conta3++)
+        for (int conta3 = 1; conta3 < cantidadCandys; conta3++)
         {
-            GameObject Cubo = Instantiate(cubo, posicion, Quaternion.identity);
-            Cubo.name = "Cubo";
+            GameObject Candy = Instantiate(candy, posicion, Quaternion.identity);
+            Candy.name = "Candy";
         }
     }
 
@@ -55,6 +57,5 @@ public class cantidadObjetos : MonoBehaviour
     {
         
     }
-
 
 }
