a82a041d2fc081521fec499cf2325996f95b103c
papermonoid <daniel.santiago.aguila.torres@gmail.com>
2020-12-14 23:08:00 -0800

    feat: psicologo form

---
+++ ar-adhd-project/Assets/Scripts/Registro/RegistroPsicologo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RegistroPsicologo : MonoBehaviour
+{
+    PsicologoDAO PsicologoDAO;
+
+    [SerializeField] InputField nombre;
+    [SerializeField] InputField cedula;
+    [SerializeField] InputField correo;
+    [SerializeField] InputField telefono;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PsicologoDAO = new PsicologoDAO();
+    }
+
+    public void Crear()
+    {
+        PsicologoDAO.Crear(new Psicologo { 
+            Nombre = nombre.text,
+            Cedula = cedula.text,
+            Correo = correo.text,
+            Telefono = telefono.text,
+            Contrasena = "SweetVictory",
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
